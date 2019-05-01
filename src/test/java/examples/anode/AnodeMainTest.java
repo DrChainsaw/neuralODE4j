fa131d1edbff966fb0afd492d79f598c045551f4
DrChainsaw <Christian.kyril.skarby@gmail.com>
2019-05-01 23:33:09 +0200

    Fix file generation and cleanup

--- src/test/java/examples/anode/AnodeMainTest.java
+++ src/test/java/examples/anode/AnodeMainTest.java
@@ -1,7 +1,13 @@
 package examples.anode;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import util.plot.NoPlot;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -12,30 +18,42 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class AnodeMainTest {
 
+
     /**
      * Test that a model can be created from scratch and trained for two epochs for 1D case.
      */
     @Test
-    public void main1D() {
-
-        final int nrofEpochs = 2;
-        final Main main = runNewMain(nrofEpochs, "-separable");
-        assertEquals("Incorrect number of epochs!", nrofEpochs, main.model.graph().getEpochCount());
+    public void main1D() throws IOException {
+        final Path baseDir = Paths.get("src", "test", "resources", "testAnodeMain1D");
+        try {
+            final int nrofEpochs = 2;
+            final Main main = runNewMain(nrofEpochs, baseDir, "-separable");
+            assertEquals("Incorrect number of epochs!", nrofEpochs, main.model.graph().getEpochCount());
+        } finally {
+            FileUtils.deleteDirectory(new File(baseDir.toString()));
+        }
     }
 
     /**
      * Test that a model can be created from scratch and trained for two epochs for 2D case.
      */
     @Test
-    public void main2D() {
-
-        final int nrofEpochs = 2;
-        final Main main = runNewMain(nrofEpochs, "-2D");
-        assertEquals("Incorrect number of epochs!", nrofEpochs, main.model.graph().getEpochCount());
+    public void main2D() throws IOException {
+        final Path baseDir = Paths.get("src", "test", "resources", "testAnodeMain2D");
+        try {
+            final int nrofEpochs = 2;
+            final Main main = runNewMain(nrofEpochs, baseDir, "-2D");
+            assertEquals("Incorrect number of epochs!", nrofEpochs, main.model.graph().getEpochCount());
+        } finally {
+            FileUtils.deleteDirectory(new File(baseDir.toString()));
+        }
     }
 
-    private Main runNewMain(int nrofEpochs, String preArg) {
-        final Main main = Main.parseArgs(preArg, "-nrofEpochs", String.valueOf(nrofEpochs), "-nrofExamples", "64", "-trainBatchSize", "64", "odenet");
+    private Main runNewMain(int nrofEpochs, Path baseDir, String preArg) {
+        final Main main = Main.parseArgs(preArg,
+                "-nrofEpochs", String.valueOf(nrofEpochs),
+                "-saveDir", baseDir.toString(),
+                "-nrofExamples", "64", "-trainBatchSize", "64", "odenet");
 
         main.plotFactory = new NoPlot.Factory();
         main.addListeners();
