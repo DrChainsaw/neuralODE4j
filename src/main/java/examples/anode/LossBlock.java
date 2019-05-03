271eff0ca4b8e8fd50b95dce3501216b2f07e5ed
DrChainsaw <Christian.kyril.skarby@gmail.com>
2019-05-03 21:41:41 +0200

    Change loss function to Huber

--- src/main/java/examples/anode/LossBlock.java
+++ src/main/java/examples/anode/LossBlock.java
@@ -2,7 +2,6 @@ package examples.anode;
 
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.nd4j.linalg.activations.impl.ActivationIdentity;
-import org.nd4j.linalg.lossfunctions.impl.LossMSE;
 
 /**
  * Adds loss layer for ANODE experiments
@@ -20,8 +19,8 @@ public class LossBlock implements Block {
                 // MSE works, but can not achieve better loss than 2/3 for 1D non-separable ODE vs 1/3 presented in paper
                 // Inner set and upper outer set is mapped to 0 while lower outer is mapped to -1.
                 // Avg MSE is mean[(1-0)^2 + (-1-0)^2 + (-1 -(-1))^2] = 2/3. L1 loss gives same result due to 1^2 = 1
-                // Huber loss?
-                .lossFunction(new LossMSE())
+                // Huber loss perhaps? It is implemented in PyTorch and will give 1/3 loss for the above case
+                .lossFunction(new LossHuber())
                 .build(), prev);
 
         return "output";
